ccda00c61ea355b7a00e06bdecd5c51b8c53d26a
Đặng Mai <itdsiu22172@student.hcmiu.edu.vn>
2024-12-01 20:22:25 +0700

    FE and BE can connect

--- BE/server.py
+++ BE/server.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template, request, jsonify
+import os
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../templates'))
+static_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../static'))  # Adjust the path
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 # Members API Route
 @app.route("/")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+def index_get():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
